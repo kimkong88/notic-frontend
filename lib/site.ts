b7c01a2ff694dfc0077a0964b86fa834707e3f2d
unknown <dkim.damian@gmail.com>
2026-02-04 01:25:32 -0800

    update url

--- lib/site.ts
+++ lib/site.ts
@@ -3,5 +3,5 @@
  * Set NEXT_PUBLIC_APP_URL to your web app (e.g. https://app.getnotic.io).
  */
 export const APP_URL =
-  process.env.NEXT_PUBLIC_APP_URL || "https://app.getnotic.io";
+    process.env.NEXT_PUBLIC_SAAS_APP_URL || "https://app.getnotic.io";
 export const DOWNLOAD_URL = "/download";
