e6b7bb6c03af172f76084affe23210a3011ff931
unknown <dkim.damian@gmail.com>
2026-02-07 02:36:54 -0800

    add clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -105,6 +105,19 @@ export default function RootLayout({
                     data-website-id="e3874e1a-bb69-4a45-bd5b-f28584930834"
                     strategy="afterInteractive"
                 />
+                {process.env.NODE_ENV === "production" &&
+                    process.env.NEXT_PUBLIC_CLARITY_ID && (
+                        <Script
+                            id="microsoft-clarity"
+                            strategy="afterInteractive"
+                        >{`
+                            (function(c,l,a,r,i,t,y){
+                                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                            })(window,document,"clarity","script","${process.env.NEXT_PUBLIC_CLARITY_ID}");
+                        `}</Script>
+                    )}
             </body>
         </html>
     );
